b742e51158db83edf3e405c5d8f0c5eb72a53680
Faker <1411430556@qq.com>
2025-06-27 19:58:27 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [['link', { rel: 'icon', href: '/茶树菇.png' }]],
+  title: '简言',
+  description: '情感故事',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.png',
+    nav: [
+      {
+        text: '导航',
+        items: [
+          { text: '主页', link: '/' },
+          { text: '情感故事', link: '/图书馆30秒.md' },
+          { text: '哔哩哔哩', link: 'https://space.bilibili.com/44113085' },
+        ],
+      },
+    ],
+
+    sidebar: [
+      {
+        text: '情感故事',
+        items: [
+          { text: '图书馆30秒', link: '/图书馆30秒.md' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/1411430556' },
+      { icon: 'bilibili', link: 'https://space.bilibili.com/44113085' },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2025-present <a href="https://github.com/1411430556">简言</a>',
+    },
+    // 设置搜索框的样式
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+            },
+          },
+        },
+      },
+    },
+  },
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "简言"
+  text: "情感故事"
+  tagline: 世上无难事，只怕有心人
+  image:
+    src: /文档.png
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: 开始
+      link: /图书馆30秒.md
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
