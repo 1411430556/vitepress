2a0c6ab4f2a6213183f9ce28b81124ab4cd797a5
Faker <1411430556@qq.com>
2025-06-27 20:58:42 +0800

    更新VitePress配置，设置基础路径为'/vitepress/'

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitepress/',
   head: [['link', { rel: 'icon', href: '/茶树菇.png' }]],
   title: '简言',
   description: '情感故事',
