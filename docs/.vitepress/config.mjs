f630b6bfc9c9205d1692abdaedfbc3e653065fbb
Faker <1411430556@qq.com>
2025-07-09 12:37:18 +0800

    🐛fix: 修复 nav 导航栏下拉菜单透明问题
    在.vitepress/config.mjs中更新了GitHub链接以指向正确的仓库，同时移除了不再使用的搜索框样式配置，优化了代码结构。在style.css中添加了新的背景颜色变量，并简化了背景渐变的定义，提升了样式的可读性和维护性。

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -81,7 +81,7 @@ const vitePressOptions = {
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/1411430556' },
+      { icon: 'github', link: 'https://github.com/1411430556/vitepress' },
       { icon: 'bilibili', link: 'https://space.bilibili.com/44113085' },
     ],
 
@@ -90,28 +90,7 @@ const vitePressOptions = {
       copyright:
         'Copyright © 2025-present <a target="_blank" href="https://github.com/1411430556">COYG⚡️</a>',
     },
-
-    // 设置搜索框的样式
-    // search: {
-    //   provider: 'local',
-    //   options: {
-    //     translations: {
-    //       button: {
-    //         buttonText: '搜索文档',
-    //         buttonAriaLabel: '搜索文档',
-    //       },
-    //       modal: {
-    //         noResultsText: '无法找到相关结果',
-    //         resetButtonTitle: '清除查询条件',
-    //         footer: {
-    //           selectText: '选择',
-    //           navigateText: '切换',
-    //           closeText: '关闭',
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
+    
     algolia,
     // 是否在 markdown 中的外部链接旁显示外部链接图标
     externalLinkIcon: true,
